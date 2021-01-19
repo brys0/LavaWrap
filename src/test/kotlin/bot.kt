bc98f89c1c8a00f194845302af27b97e0624910c
Bryson <brysontenhaken@gmail.com>
2021-01-18 22:28:19 -0600

    add initial

---
+++ src/test/kotlin/bot.kt
@@ -0,0 +1,7 @@
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.OnlineStatus
+import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.requests.GatewayIntent
+
+fun main(args: Array<String>) {
+}
